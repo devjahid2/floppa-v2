2f8a4496009a59d34d6890764832b80f377b8d27
Jahid <jahidhasan.developer@gmail.com>
2023-05-09 22:16:32 +0700

    update

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -7,21 +7,21 @@ import { SiGitbook } from 'react-icons/si';
 
 const Social = () => {
     return (
-        <div className='d-flex flex-column justify-content-center flex-md-row align-items-start align-items-md-center  gap-3'>
-            <div className="d-flex align-items-center gap-2">
+        <div className='d-flex flex-row justify-content-between justify-content-md-center align-items-start align-items-md-center social-wrap gap-3'>
+            <div className="d-flex flex-md-row flex-column align-items-start gap-3">
             <SocialButton url={'https://t.me/BigFloppaPortal'} title={'Telegram'}>
                 <BsTelegram className='font-20' />
             </SocialButton>
-            <SocialButton url={'https://twitter.com/floppa_eth'} title={'Twitter'}>
-                <AiFillTwitterCircle className='font-20'/>
+            <SocialButton url={'https://domain.com'} title={'Book'}>
+                <SiGitbook className='font-20'/>
             </SocialButton>
             </div>
-            <div className="d-flex align-items-center gap-2">
+            <div className="d-flex flex-md-row flex-column align-items-start gap-3">
             <SocialButton url={'https://medium.com/@BigFloppa'} title={'Medium'}>
                 <BsMedium className='font-20'/>
             </SocialButton>
-            <SocialButton url={'https://domain.com'} title={'Book'}>
-                <SiGitbook className='font-20'/>
+            <SocialButton url={'https://twitter.com/floppa_eth'} title={'Twitter'}>
+                <AiFillTwitterCircle className='font-20'/>
             </SocialButton>
             </div>
         </div>
